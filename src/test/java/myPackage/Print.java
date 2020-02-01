ccc576c2627042233a84273b466d6d667b4bf326
Manoj KB <36417972+manojkbkumar@users.noreply.github.com>
2020-02-01 16:39:31 +0530

    Create Print.java

---
+++ src/test/java/myPackage/Print.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+import org.testng.annotations.Test;
+
+public class Print {
+@Test
+private void syso() {
+	System.out.println("Your or My Print?");
+	System.out.println("My Print");
+
+}
+}
+© 2020 GitHub, Inc.
